dc7b22fb11b3783e4df7dcab2c4bb897e364f285
Arthur Puthin <arthur.puthin@ilegra.com>
2017-01-17 18:16:20 -0200

    up to lesson 53 - virtual properties

--- test/update_test.js
+++ test/update_test.js
@@ -6,7 +6,7 @@ describe('Updates users', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -53,11 +53,11 @@ describe('Updates users', () => {
     );
   });
 
-  xit('a user can have their postcount inc by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+  it('a user can have their likes inc by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 10);
         done();
       });
   });
