3a30c4ee257c4f5a2b09b3ac2ee0a43cc74e319a
Arthur Puthin <arthur.puthin@ilegra.com>
2017-01-19 10:52:20 -0200

    UpStarMusic complete, up to lesson 95

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,62 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist.find(buildQuery(criteria));
+
+  /* my half-working solution */
+  //const query = Artist.find({});
+    
+  // if (criteria.name !== undefined && criteria.name.length > 0) {
+  //   query.where('name').text(criteria.name);
+  // }
+
+  // if (criteria.age !== undefined) {
+  //   query.where('age')
+  //     .gte(criteria.age.min)
+  //     .lte(criteria.age.max);
+  // }
+
+  // if (criteria.yearsActive !== undefined) {
+  //   query.where('yearsActive')
+  //     .gte(criteria.yearsActive.min)
+  //     .lte(criteria.yearsActive.max);
+  // }
+
+  query.sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count(buildQuery(criteria))])
+    .then((result) => {
+      return { 
+        all: result[0], 
+        count: result[1], 
+        offset: offset, 
+        limit: limit 
+      };
+    });
 };
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    }
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    }
+  }
+
+  return query;
+}
