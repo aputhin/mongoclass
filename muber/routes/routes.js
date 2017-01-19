ad4edeac4039b67e22767b2bc38652d862c23fc2
Arthur Puthin <arthur.puthin@ilegra.com>
2017-01-19 16:50:07 -0200

    Course complete, muber tests working

--- muber/routes/routes.js
+++ muber/routes/routes.js
@@ -2,5 +2,9 @@ const DriversController = require('../controllers/drivers_controller');
 
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
-  app.post('/api', DriversController.create);
+  app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.update);
+  app.delete('/api/drivers/:id', DriversController.remove);
+
+  app.get('/api/drivers', DriversController.index);
 }
---
+++ muber/test/controllers/drivers_controller_test.js
@@ -0,0 +1,76 @@
+const assert = require('assert');
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../../app');
+const Driver = mongoose.model('driver');
+
+describe('Drivers controller', () => {
+  it('POST to /api/drivers creates a new driver', (done) => {
+    Driver.count().then(count => {
+      request(app)
+        .post('/api/drivers')
+        .send({ email: 'test@test.com' })
+        .end((err, response) => {
+          Driver.count().then(newCount => {
+            assert(count + 1 === newCount);
+            done();
+          });
+        });
+    });
+  });
+
+  it('PUT to /api/drivers/:id updates a driver', (done) => {
+    const driver = new Driver({ email: 't@oy.com', driving: false });
+    
+    driver.save().then(() => {
+      request(app)
+        .put(`/api/drivers/${driver._id}`)
+        .send({ driving: true })
+        .end(() => {
+          Driver.findOne({ email: 't@oy.com' })
+            .then(driver => {
+              assert(driver.driving === true);
+              done();
+            })
+        });
+    });
+  });
+
+  it('DELETE to /api/drivers/:id removes a driver', (done) => {
+    const driver = new Driver({ email: 't@oy.com' });
+    
+    driver.save().then(() => {
+      request(app)
+        .delete(`/api/drivers/${driver._id}`)
+        .end(() => {
+          Driver.findOne({ email: 't@oy.com' })
+            .then(driver => {
+              assert(driver === null);
+              done();
+            });
+        });
+    });
+  });
+
+  it('GETS near drivers from /api/drivers', (done) => {
+    const seattleDriver = new Driver({
+      email: 'seattle@test.com',
+      geometry: { type: 'Point', coordinates: [-122.4759902, 47.6147628] }
+    });
+    const miamiDriver = new Driver({
+      email: 'miami@test.com',
+      geometry: { type: 'Point', coordinates: [-80.252, 25.791] }
+    });
+
+    Promise.all([ seattleDriver.save(), miamiDriver.save() ])
+      .then(() => {
+        request(app)
+          .get('/api/drivers?lng=-80&lat=25')
+          .end((err, response) => {
+            assert(response.body.length === 1);
+            assert(response.body[0].obj.email === 'miami@test.com');
+            done();
+          });
+      })
+  });
+});
